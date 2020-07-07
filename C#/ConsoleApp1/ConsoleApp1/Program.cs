94c6970ed2b9735624b12991cbeff1a5912262eb
Stepan St <Star.starodubtsev13@gmail.com>
2020-07-07 22:04:10 +0300

    Release

--- C#/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,37 +10,9 @@ namespace Homework
     class Program
     {
         static void Main(string[] args)
-        { 
-            int[] test_FGN_1 = new int[] { 6, 1, 2, 3, 4, 5, 6 }; //-1
-            int[] test_FGN_2 = new int[] { 1, 4, 4, 4, 4, 5, 6 }; //4
-            int[] test_FGN_3 = new int[] { 0, 0, 0, 0, 0, 0, 0 }; //0
-            int[] test_FGN_4 = new int[] { 1 };                   //1
-            Console.WriteLine(HW_1.FindGuideNumber(test_FGN_1));
-            Console.WriteLine(HW_1.FindGuideNumber(test_FGN_2));
-            Console.WriteLine(HW_1.FindGuideNumber(test_FGN_3));
-            Console.WriteLine(HW_1.FindGuideNumber(test_FGN_4));
+        {
 
 
-
-            int[] test_FMI_1 = new int[] { -4, -4, -4, 4, -4, -4, -4 }; //2,3,4
-            int[] test_FMI_2 = new int[] { 3, -4, -2, 0, -3 };//3
-            Console.WriteLine(HW_1.FindMainIndexes(test_FMI_1));
-            Console.WriteLine(HW_1.FindMainIndexes(test_FMI_2));
-
-
-
-            int[] test_FV_1 = new int[] { 2, 1, 2, 1, 2, 1, 2 }; //3
-            int[] test_FV_2 = new int[] { 1, 5, 1, 3, 1, 2, 1, 4 }; //12
-            Console.WriteLine(HW_1.FindVolume(test_FV_1));
-            Console.WriteLine(HW_1.FindVolume(test_FV_2));
-
-
-            string old_word = new string("capybara");
-            string new_word = new string("time-traveler");
-            string test_Cens_1 = new string("I see a capybara at forest");
-            string test_Cens_2 = new string("Where is my capybara-backpack for that capybara?");
-            Console.WriteLine(HW_1.Censor(test_Cens_1, old_word,new_word));
-            Console.WriteLine(HW_1.Censor(test_Cens_2, old_word,new_word));
         }
 
     }
@@ -50,8 +22,8 @@ namespace Homework
         static public string Censor(string str, string old_word, string new_word) //capybara -> copypast
         {
             string new_str = new string("");
-            int ending=0;
-            for (int i = 0; i < str.Length-old_word.Length; i++)
+            int ending = 0;
+            for (int i = 0; i < str.Length - old_word.Length; i++)
             {
                 if (str[i..(old_word.Length + i)].Equals(old_word))
                 {
@@ -59,10 +31,11 @@ namespace Homework
                     i += old_word.Length;
                 }
                 new_str += str[i];
-                ending = i+1;
+                ending = i + 1;
             }
-            for(int i = ending;i<str.Length;i++){
-                new_str+=str[i];
+            for (int i = ending; i < str.Length; i++)
+            {
+                new_str += str[i];
             }
 
             return new_str;
@@ -133,6 +106,169 @@ namespace Homework
             }
 
             return volume;
+        }
+    }
+    class Test_HW_1
+    {
+        static public void Test_Cens()
+        {
+            string old_word = new string("capybara");
+            string new_word = new string("time-traveler");
+            string test_Cens_1 = new string("I see a capybara at forest");
+            string test_Cens_2 = new string("Where is my capybara-backpack for that capybara?");
+            Console.WriteLine(HW_1.Censor(test_Cens_1, old_word, new_word));
+            Console.WriteLine(HW_1.Censor(test_Cens_2, old_word, new_word));
+        }
+        static public void Test_FGN()
+        {
+            int[] test_FGN_1 = new int[] { 6, 1, 2, 3, 4, 5, 6 }; //-1
+            int[] test_FGN_2 = new int[] { 1, 4, 4, 4, 4, 5, 6 }; //4
+            int[] test_FGN_3 = new int[] { 0, 0, 0, 0, 0, 0, 0 }; //0
+            int[] test_FGN_4 = new int[] { 1 };                   //1
+            Console.WriteLine(HW_1.FindGuideNumber(test_FGN_1));
+            Console.WriteLine(HW_1.FindGuideNumber(test_FGN_2));
+            Console.WriteLine(HW_1.FindGuideNumber(test_FGN_3));
+            Console.WriteLine(HW_1.FindGuideNumber(test_FGN_4));
+        }
+        static public void Test_FMI()
+        {
+            int[] test_FMI_1 = new int[] { -4, -4, -4, 4, -4, -4, -4 }; //2,3,4
+            int[] test_FMI_2 = new int[] { 3, -4, -2, 0, -3 };//3
+            Console.WriteLine(HW_1.FindMainIndexes(test_FMI_1));
+            Console.WriteLine(HW_1.FindMainIndexes(test_FMI_2));
+        }
+        static public void Test_FV()
+        {
+            int[] test_FV_1 = new int[] { 2, 1, 2, 1, 2, 1, 2 }; //3
+            int[] test_FV_2 = new int[] { 1, 5, 1, 3, 1, 2, 1, 4 }; //12
+            Console.WriteLine(HW_1.FindVolume(test_FV_1));
+            Console.WriteLine(HW_1.FindVolume(test_FV_2));
+        }
+    }
+    class Matrix
+    {
+        static private int rank = 2;
+        static private double[,] matrix = new double[rank, rank];
+        //  00 01
+        //  10 11
+        static private double determinator;
+
+        public double get_determinator()
+        {
+            Determinate();
+            return determinator;
+        }
+        static public void Determinate()
+        {
+            determinator = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+        }
+        public double this[int x, int y]
+        {
+            get
+            {
+                return matrix[x, y];
+            }
+            set
+            {
+                matrix[x, y] = value;
+            }
+        }
+        public Matrix FindReverseMatrix()
+        {
+            Matrix reverse = new Matrix();
+            Determinate();
+            reverse[0, 0] = matrix[1, 1] / determinator;
+            reverse[0, 1] = -matrix[0, 1] / determinator;
+            reverse[1, 0] = -matrix[1, 0] / determinator;
+            reverse[1, 1] = matrix[0, 0] / determinator;
+            return reverse;
+        }
+        static public Matrix operator +(Matrix one, Matrix two)
+        {
+            Matrix sum = new Matrix();
+            sum[0, 0] = one[0, 0] + two[0, 0];
+            sum[1, 0] = one[1, 0] + two[1, 0];
+            sum[0, 1] = one[0, 1] + two[0, 1];
+            sum[1, 1] = one[1, 1] + two[1, 1];
+            return sum;
+        }
+        static public bool operator <(Matrix one, Matrix two)
+        {
+            return one.get_determinator() < two.get_determinator() ? true : false;
+        }
+        static public bool operator >(Matrix one, Matrix two)
+        {
+            return one.get_determinator() > two.get_determinator() ? true : false;
+        }
+        static public bool operator ==(Matrix one, Matrix two)
+        {
+            return one[0, 0] == two[0, 0] && one[0, 1] == two[0, 1] && one[1, 0] == two[1, 0] && one[1, 1] == two[1, 1];
+        }
+        static public bool operator !=(Matrix one, Matrix two)
+        {
+            return !(one[0, 0] == two[0, 0] && one[0, 1] == two[0, 1] && one[1, 0] == two[1, 0] && one[1, 1] == two[1, 1]);
+        }
+        static public Matrix operator *(Matrix one, Matrix two)
+        {
+            Matrix result = new Matrix();
+            result[0, 0] = one[0, 0] * two[0, 0] + one[0, 1] * two[1, 0];
+            result[0, 1] = one[0, 0] * two[0, 1] + one[0, 1] * two[1, 1];
+            result[1, 0] = one[1, 0] * two[0, 0] + one[1, 1] * two[1, 0];
+            result[1, 1] = one[1, 0] * two[0, 1] + one[1, 1] * two[1, 1];
+            return result;
+        }
+
+        static public double operator /(Matrix one, Matrix two)
+        {
+            return one.get_determinator() / two.get_determinator();
+        }
+        public void Parse(string str)
+        {
+            // 00 01 10 11
+            string[] str_array = str.Split(" ", 4);
+            matrix[0, 0] = double.Parse(str_array[0]);
+            matrix[0, 1] = double.Parse(str_array[1]);
+            matrix[1, 0] = double.Parse(str_array[2]);
+            matrix[1, 1] = double.Parse(str_array[3]);
+        }
+        public bool TryParse(string str, out Matrix matr)
+        {
+            string[] str_array = str.Split(" ", 4);
+            byte result = 0;
+            double number;
+            Matrix tmp = new Matrix();
+
+            if (double.TryParse(str_array[0], out number))
+            {
+                result++;
+                tmp[0, 0] = number;
+            }
+            if (double.TryParse(str_array[1], out number))
+            {
+                result++;
+                tmp[0, 1] = number;
+            }
+            if (double.TryParse(str_array[2], out number))
+            {
+                result++;
+                tmp[1, 0] = number;
+            }
+            if (double.TryParse(str_array[3], out number))
+            {
+                result++;
+                tmp[1, 1] = number;
+            }
+            if (result == 4)
+            {
+                matr = tmp;
+                return true;
+            }
+            else
+            {
+                tmp[0,0]=tmp[0,1]=tmp[1,0]=tmp[1,1]=0;
+                matr = tmp;
+                return false;
+            }
         }
     }
 }
